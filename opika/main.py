d7328366dd6b2401c280674138695b98a88cc39d
sanix-darker <s4nixd@gmail.com>
2022-07-10 11:51:35 +0200

    feat; create main as a module in opika

--- opika/main.py
+++ opika/main.py
@@ -1,27 +1,27 @@
-import keyboard
-from os import system
-from threading import Thread
 import time
+from os import system
 from random import randint
+from threading import Thread
 
+import keyboard
 
-KEYS = ['Down', 'Up', 'Left', 'Right']
+KEYS = ["Down", "Up", "Left", "Right"]
 
 WIDTH = 70
 HEIGHT = 50
-ME = '🐼'
+ME = "🐼"
 pos_x, pos_y = randint(0, WIDTH - 5), randint(0, HEIGHT - 5)
 
-SCENE = [[' ' for y in range(HEIGHT)] for x in range(WIDTH)]
+SCENE = [[" " for y in range(HEIGHT)] for x in range(WIDTH)]
 SCENE[pos_x][pos_y] = ME
 SCORE = 0
 
-BALL = '🥚'
+BALL = "🥚"
 ball_x, ball_y = randint(0, WIDTH - 5), randint(0, HEIGHT - 5)
 
 ENV_GENERATED = False
 
-PNJS = ['🐄', '🐑', '🦆', '🐓', '🐃', '🐂', '🦏', '🐎', '🐖', '🦍', '🐆']
+PNJS = ["🐄", "🐑", "🦆", "🐓", "🐃", "🐂", "🦏", "🐎", "🐖", "🦍", "🐆"]
 
 max_time = 100
 
@@ -31,7 +31,8 @@ def map_it(elt, x, y):
 
 
 def clean_it(x, y):
-    SCENE[x][y] = ' '
+    SCENE[x][y] = " "
+
 
 def map_it_and_clean(elt, x0, y0, x1, y1):
     clean_it(x0, y0)
@@ -47,13 +48,13 @@ def generate_env():
 
     if not ENV_GENERATED:
         for i in range(randint(5, 20)):
-            map_it_randomly('🌲')
-            map_it_randomly('🌳')
+            map_it_randomly("🌲")
+            map_it_randomly("🌳")
 
         for i in range(randint(2, 10)):
-            map_it_randomly('🪨')
+            map_it_randomly("🪨")
 
-        for i in range(randint(5, 20)):
+        for i in range(randint(5, 30)):
             map_it_randomly(PNJS[randint(0, len(PNJS) - 1)])
 
         ENV_GENERATED = True
@@ -105,20 +106,37 @@ def move_down():
         pos_y += 1
 
 
+def move_pnjs(x, y):
+    global SCENE
+
+    while True:
+        for j in range(0, HEIGHT - 1):
+            for i in range(0, WIDTH - 1):
+                if SCENE[i][i] in PNJS:
+                    randd = randint(1, 100)
+                    if randd % 2 == 0:
+                        map_it_and_clean(SCENE[i][j], i, j, i + 1, j)
+                    elif randd % 3 == 0 or randd % 5 == 0:
+                        map_it_and_clean(SCENE[i][j], i, j, i - 1, j)
+                    time.sleep(1)
+
+        time.sleep(0.3)
+
+
 def refresh_scene():
     global SCENE, max_time
 
     while True:
-        rend_this = ''
+        rend_this = ""
         for j in range(0, HEIGHT - 1):
             for i in range(0, WIDTH - 1):
                 rend_this += SCENE[i][j] + ' '
             rend_this += '\n'
 
         print(rend_this)
-        print(f'{pos_x}/{pos_y} => score : {SCORE} | {max_time}')
-        print('______________________________________________')
-        time.sleep(0.04)
+        print(f'{pos_x}/{pos_y} => score : {SCORE} | {max_time}s')
+        print('_' * 100)
+        time.sleep(0.033)
 
         system('clear')
 
@@ -127,7 +145,6 @@ def render(key):
     global KEYS, SCENE, SCORE, max_time
 
     while True:
-        generate_env()
         make_ball()
 
         keyboard.wait(key)
@@ -153,13 +170,19 @@ def timer_decr():
         max_time -= 1
 
 
-thread_scene = Thread(target=refresh_scene)
-thread_scene.start()
+if __name__ == "__main__":
+    generate_env()
 
-thread_time = Thread(target=timer_decr)
-thread_time.start()
+    thread_scene = Thread(target=refresh_scene)
+    thread_scene.start()
 
-threads = [Thread(target=render, kwargs={'key': key}) for key in KEYS]
+    thread_pnjs = Thread(target=move_pnjs)
+    thread_pnjs.start()
 
-for thread in threads:
-    thread.start()
+    thread_time = Thread(target=timer_decr)
+    thread_time.start()
+
+    threads = [Thread(target=render, kwargs={'key': key}) for key in KEYS]
+
+    for thread in threads:
+        thread.start()
